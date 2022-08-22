7f3d4cb2b234416a2a0b8107061dd2aaca902325
hanjoondev <81179488+hanjoondev@users.noreply.github.com>
2022-08-23 05:00:02 +0900

    Initial Commit Ranking (#11)
    난이도별 랭킹 조회 기능 w/ paging

--- src/main/java/com/zerobase/minesweeper/repository/GameRepository.java
+++ src/main/java/com/zerobase/minesweeper/repository/GameRepository.java
@@ -2,10 +2,13 @@ package com.zerobase.minesweeper.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.zerobase.minesweeper.entity.Game;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends PagingAndSortingRepository<Game, Long> {
     List<Game> findFirst10ByGamerIdOrderByCreatedAtDesc(Long gamerId);
+    Page<Game> findByDifficulty(String difficulty, Pageable pageable);
 }
--- src/main/java/com/zerobase/minesweeper/service/GameService.java
+++ src/main/java/com/zerobase/minesweeper/service/GameService.java
@@ -1,17 +1,25 @@
 package com.zerobase.minesweeper.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.zerobase.minesweeper.dto.GameRequest;
 import com.zerobase.minesweeper.dto.GameResponse;
 import com.zerobase.minesweeper.dto.GetGameListResponse;
 import com.zerobase.minesweeper.dto.GetGameResponse;
+import com.zerobase.minesweeper.dto.RankingResponse;
+import com.zerobase.minesweeper.dto.Rank;
 import com.zerobase.minesweeper.entity.Game;
 import com.zerobase.minesweeper.entity.Gamer;
 import com.zerobase.minesweeper.repository.GameRepository;
@@ -82,12 +90,39 @@ public class GameService {
         Optional<Gamer> optionalGamer = gamerRepository.findById(id);
         List<Game> games = gameRepository.findFirst10ByGamerIdOrderByCreatedAtDesc(id);
         return GetGameListResponse.builder()
-                                  .gamerId(id)
-                                  .errorCode(optionalGamer.isEmpty() ? 1 : games.size() == 0 ? 2 : 0)
-                                  .gameList(games.stream().map(i -> i.getId()).collect(Collectors.toList()))
-                                  .difficultyList(games.stream().map(i -> i.getDifficulty()).collect(Collectors.toList()))
-                                  .timeList(games.stream().map(i -> i.getTimePlayed()).collect(Collectors.toList()))
-                                  .createdAtList(games.stream().map(i -> i.getCreatedAt().toString()).collect(Collectors.toList()))
-                                  .build();
+                .gamerId(id)
+                .errorCode(optionalGamer.isEmpty() ? 1 : games.size() == 0 ? 2 : 0)
+                .gameList(games.stream().map(i -> i.getId()).collect(Collectors.toList()))
+                .difficultyList(games.stream().map(i -> i.getDifficulty()).collect(Collectors.toList()))
+                .timeList(games.stream().map(i -> i.getTimePlayed()).collect(Collectors.toList()))
+                .createdAtList(games.stream().map(i -> i.getCreatedAt().toString()).collect(Collectors.toList()))
+                .build();
+    }
+
+    public RankingResponse getRanking(String difficulty, Integer pageIdx, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageIdx, pageSize, Sort.by("timePlayed").ascending());
+        Page<Game> page = gameRepository.findByDifficulty(difficulty, pageable);
+        RankingResponse response = RankingResponse.builder()
+                                                  .difficulty(difficulty)
+                                                  .firstPage(page.isFirst())
+                                                  .lastPage(page.isLast())
+                                                  .numberOfElements(page.getNumberOfElements())
+                                                  .totalElements(page.getTotalElements())
+                                                  .contents(new ArrayList<>())
+                                                  .build();
+        HashMap<Long, String> names = new HashMap<>();
+        Integer numItems = page.getNumberOfElements();
+        Long start = page.getPageable().getOffset() + 1;
+        for (int i = 0; i < numItems; i++) {
+            Long gameId = page.getContent().get(i).getId(), gamerId = page.getContent().get(i).getGamerId();
+            Boolean isAnon = page.getContent().get(i).getIsAnon();
+            Double timePlayed = page.getContent().get(i).getTimePlayed();
+            names.computeIfAbsent(gamerId,
+                    k -> isAnon ? "anonymous" + String.valueOf(gamerId)
+                                : gamerRepository.findById(gamerId).get().getName());
+            response.getContents()
+                    .add(new Rank(start + i, names.get(gamerId), timePlayed, gameId, gamerId));
+        }
+        return response;
     }
 }
