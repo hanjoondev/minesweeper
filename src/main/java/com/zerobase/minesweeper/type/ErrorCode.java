29303a413e8343ad856410b055ec3f9685b9c63a
Mike-Jeong <alstlr65@gmail.com>
2022-08-25 16:25:36 +0900

    refactor : GamerService Error Handling 수정 Close #12 GamerService Error 핸들링 변경

--- src/main/java/com/zerobase/minesweeper/type/ErrorCode.java
+++ src/main/java/com/zerobase/minesweeper/type/ErrorCode.java
@@ -9,14 +9,17 @@ public enum ErrorCode {
 
     USER_NOT_FOUND("사용자가 없습니다."),
     NOT_AUTHENTICATED_EMAIL("이메일 인증이 되지 않은 사용자입니다."),
+    USER_ALREADY_VALIDATED("이미 이메일 인증이 된 사용자 입니다."),
+    VALIDATION_KEY_MIS_MATCH("올바르지 않은 인증키 입니다."),
+    PASSWORD_MIS_MATCH("올바르지 않은 비밀번호 입니다."),
     INVALID_LOGIN_INFO("아이디 또는 비밀번호가 일치하지 않습니다."),
-
-
     NOT_FOUND_USER_TOKEN("로그아웃된 사용자입니다."),
     MIS_MATCH_TOKEN("토큰 정보가 일치하지 않습니다."),
     INVALID_REFRESH_TOKEN("refresh token이 유효하지 않습니다. 재로그인이 필요합니다."),
     INVALID_TOKEN("token 값이 유효하지 않습니다."),
-    NEED_TO_ROLE_ADMIN("관리자 권한이 필요합니다"),
+    NEED_TO_ROLE_ADMIN("관리자 권한이 필요합니다."),
+
+    INTERNAL_SERVER_ERROR("서버에 이상이 생겼습니다")
     ;
 
     private final String description;
